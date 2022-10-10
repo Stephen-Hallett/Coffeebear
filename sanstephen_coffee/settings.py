b8f341fb988f679a14c1b2a7757feac34abf1a16
Stephen-Hallett <stevohallett@gmail.com>
2022-10-10 17:25:10 +1300

    allowed hosts updated

--- sanstephen_coffee/settings.py
+++ sanstephen_coffee/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [""]
+ALLOWED_HOSTS = [".herokuapp.com",
+                    ".herokuapp.com."]
 
 
 # Application definition
