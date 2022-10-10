26ef43569e4576fc7c418b80efc07ea6e3570720
Stephen-Hallett <stevohallett@gmail.com>
2022-10-10 18:11:21 +1300

    edits to settings

--- sanstephen_coffee/settings.py
+++ sanstephen_coffee/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".coffeebear.herokuapp.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
